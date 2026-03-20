d8f73f0e3fd4d4648393f2a4a783b273f1d2f0c7
Sven Van Caekenberghe <sven@stfx.eu>
2026-03-20 14:11:29 +0100

    added ex_doc support

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule SPQueue.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # hex
+      package: package(),
+      description: "A Simple Persistent Queue",
+
+      # docs
+      name: "SPQueue",
+      source_url: "https://github.com/svenvc/spqueue",
+      docs: docs()
     ]
   end
 
@@ -21,8 +30,23 @@ defmodule SPQueue.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "SPQueue",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package do
+    [
+      licenses: "MIT",
+      links: %{
+        "GitHub" => "https://github.com/svenvc/spqueue"
+      }
     ]
   end
 end
