60e90d8046c774e56d4d8a35ec7999414183b516
Sven Van Caekenberghe <sven@stfx.eu>
2026-03-31 13:27:13 +0200

    move to 0.1.5

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule SPQueue.MixProject do
   def project do
     [
       app: :spqueue,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
